a4a3d123e8515ed24b1c41787e3ee68440348b6b
ayden <aydendiel@gmail.com>
2025-08-08 13:00:14 -0400

    feat: got basic proof-of-concept working

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+from src.openai import get_openai_response
 from src.tts import tts 
 from src.stt import await_speech_command, stt_from_mic, init_mic
 
 def callback():
-    print("triggered result")
+    tts("How can I help you?")
+    prompt = stt_from_mic(10)
+    response = get_openai_response(prompt)
+    tts(response)
     exit(0)
 
 init_mic()
+tts("Starting up. Say, \"Hey, robot\" and ask a question")
 
 await_speech_command(callback)
 
