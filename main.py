2f31ac3b81e3861ab187bbaa8d2c3583ddbb8d00
ayden <aydendiel@gmail.com>
2025-08-08 12:41:47 -0400

    feat: added basic voice trigger

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from src.tts import tts 
-from src.stt import stt_from_mic
+from src.stt import await_speech_command, stt_from_mic, init_mic
 
-tts(stt_from_mic())
+def callback():
+    print("triggered result")
+    exit(0)
+
+init_mic()
+
+await_speech_command(callback)
+
--- src/stt.py
+++ src/stt.py
@@ -8,25 +8,43 @@ model = whisper.load_model("tiny.en")
 def stt_from_file(file_path="output.wav") -> str:
     return str(model.transcribe(file_path)['text'])
 
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
+CHUNK = 2048
 RATE = 44100
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
 WAVE_OUTPUT_FILENAME = "output.wav"
 
-p = pyaudio.PyAudio()
+device_index = 0
+
+def _print_available_devices():
+    p = pyaudio.PyAudio()
+    print("Available Input Devices:")
+    default_api_index = p.get_default_host_api_info()["index"]
+    info = p.get_host_api_info_by_index(default_api_index)
+    numdevices = int(info.get('deviceCount'))
+    for i in range(numdevices):
+        device_info = p.get_device_info_by_host_api_device_index(default_api_index, i)
+        if device_info.get('maxInputChannels') > 0:
+            print(f"Input Device ID: {i} - Name: {device_info.get('name')}")
+
+def init_mic():
+    _print_available_devices()
+    global device_index 
+    device_index = int(input("Which device id to use >"))
 
 def stt_from_mic(record_seconds=5, file_path=WAVE_OUTPUT_FILENAME) -> str:
+    p = pyaudio.PyAudio()
     stream = p.open(format=FORMAT,
                     channels=CHANNELS,
                     rate=RATE,
                     input=True,
-                    frames_per_buffer=CHUNK)
+                    frames_per_buffer=CHUNK,
+                    input_device_index=device_index)
 
     frames = []
 
     for i in range(0, int(RATE / CHUNK * record_seconds)):
-        data = stream.read(CHUNK)
+        data = stream.read(CHUNK, exception_on_overflow=False)
         frames.append(data)
 
     stream.stop_stream()
@@ -43,3 +61,10 @@ def stt_from_mic(record_seconds=5, file_path=WAVE_OUTPUT_FILENAME) -> str:
     os.remove(file_path)
 
     return result
+
+def await_speech_command(callback, command="robot"):
+    chunk_seconds = 5
+    while True:
+        speech = stt_from_mic(chunk_seconds)
+        if command in speech.lower():
+            callback()
